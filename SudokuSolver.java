8974220a4299abd4361e546a1003742a7fe5d96f
Abdul <muizabdul216@gmail.com>
2024-02-28 17:57:29 -0500

    add main java file

---
+++ SudokuSolver.java
@@ -0,0 +1,18 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        /*
+         * Input: Sudoku .txt file and desired algorithm.
+         * Sudoku file will be a .txt file with the following format:
+         * .34..786.
+         * ..1.3..7.
+         * 8....1...
+         * 5...48...
+         * ..69..75.
+         * .1......9
+         * 6.....13.
+         * ....6..84
+         * ......5..
+         */
+
+    }
+}
