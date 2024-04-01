740f5e4863a2995dbce72e881c02d16892d428e1
therealkevind <47948540+therealkevind@users.noreply.github.com>
2024-04-01 16:20:37 -0400

    add timing to logical algos

--- tests/logic.sh
+++ tests/logic.sh
@@ -1,6 +1,6 @@
 cd $(dirname "$0")/..
 for i in tests/logic*.in.txt; do
-  if java -ea -cp bin SudokuSolver "$i" "$@" | tail -n +2 | diff --strip-trailing-cr "${i%.in.txt}.out.txt" -; then
+  if java -ea -cp bin SudokuSolver "$i" "$@" | tail -n +2 | head -n -1 | diff --strip-trailing-cr "${i%.in.txt}.out.txt" -; then
     echo "$i passed!"
   else
     echo "$i failed!"
